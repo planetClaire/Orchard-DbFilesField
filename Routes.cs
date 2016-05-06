1b5a64fedb207ffbe26a1b5a539a145cd18660e9
marshy <marshy@zeta.com.au>
2016-05-06 11:25:44 +0800

    tell browsers the file name http://stackoverflow.com/a/339025/307455

---
+++ Routes.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Orchard.Mvc.Routes;
+
+namespace DbFilesField
+{
+    public class Routes : IRouteProvider
+    {
+        public IEnumerable<RouteDescriptor> GetRoutes()
+        {
+            return new[] {
+                new RouteDescriptor {
+                    Priority = 100,
+                    Route = new Route(
+                        "DBFilesField/Download/{filename}",
+                        new RouteValueDictionary {
+                            {"area", "DBFilesField"},
+                            {"controller", "Download"},
+                            {"action", "Index"}
+                        },
+                        new RouteValueDictionary(),
+                        new RouteValueDictionary {
+                            {"area", "DBFilesField"}
+                        },
+                        new MvcRouteHandler())
+                    }
+
+            };
+        }
+
+        public void GetRoutes(ICollection<RouteDescriptor> routes)
+        {
+            foreach (var routeDescriptor in GetRoutes())
+            {
+                routes.Add(routeDescriptor);
+            }
+        }
+    }
+}
